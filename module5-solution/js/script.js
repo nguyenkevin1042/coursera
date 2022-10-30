8a509f844d088009c42bcd37a5c277c256e6224c
Tien Nguyen <17130244@st.hcmuaf.edu.vn>
2022-10-30 15:46:05 +0700

    completed module5

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -13,7 +13,7 @@ $(function(){
 	var davidChu = {};
 
 	//url for home page
-	var homeHtml = "snippets/home-snippet.html";
+	var homeHtmlUrl = "snippets/home-snippet.html";
 	//url for menu page
 	var allCategoriesUrl =
   		"https://davids-restaurant.herokuapp.com/categories.json";
@@ -47,34 +47,83 @@ $(function(){
 	}
 
 	/*SHOW ACTIVE WHEN BEING IN SPECIFIC PAGE*/
-var switchMenuToActive = function (){
-	var classes = document.querySelector("#navHomeButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navHomeButton").className = classes;
+	var switchMenuToActive = function (){
+		var classes = document.querySelector("#navHomeButton").className;
+	  classes = classes.replace(new RegExp("active", "g"), "");
+	  document.querySelector("#navHomeButton").className = classes;
 
-  // Add 'active' to menu button if not already there
-  classes = document.querySelector("#navMenuButton").className;
-  if (classes.indexOf("active") == -1) {
-    classes += " active";
-    document.querySelector("#navMenuButton").className = classes;
-  } 
-};
+	  // Add 'active' to menu button if not already there
+	  classes = document.querySelector("#navMenuButton").className;
+	  if (classes.indexOf("active") == -1) {
+	    classes += " active";
+	    document.querySelector("#navMenuButton").className = classes;
+	  } 
+	};
 
-	/* Show home page*/
+	/* Show home page with random category in special tile*/
 	document.addEventListener("DOMContentLoaded", function (event) {
 		showLoading("#main-content");
-		$ajaxUtils.
-		sendGetRequest(
-			homeHtml,
-			function(responseText){
-				insertHtml("#main-content",responseText)
-			},
-			false);
+		$ajaxUtils.sendGetRequest(
+  		allCategoriesUrl, 
+  		buildAndShowHomeHTML, 
+  		true);
 	});
 
-	
+	// Builds HTML for the home page based on categories array
+	// returned from the server.
+	function buildAndShowHomeHTML (categories) {
+	  
+	  // Load home snippet page
+	  $ajaxUtils.sendGetRequest(
+	    homeHtmlUrl,
+	    function (homeHtml) {
+	      // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
+	      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
+	      // variable's name implies it expects.
 
-	
+	      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
+	      // chosen category from STEP 2. Use existing insertProperty function for that purpose.
+	      // Look through this code for an example of how to do use the insertProperty function.
+	      // WARNING! You are inserting something that will have to result in a valid Javascript
+	      // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
+	      // being passed into the $dc.loadMenuItems function. Think about what that argument needs
+	      // to look like. For example, a valid call would look something like this:
+	      // $dc.loadMenuItems('L')
+	      // Hint: you need to surround the chosen category short name with something before inserting
+	      // it into the home html snippet.
+	      // var homeHtmlToInsertIntoMainPage =
+
+	      // insertProperty(homeHtmlUrl,"randomCategoryShortName","SP");
+	    
+	      // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
+	      // Use the existing insertHtml function for that purpose. Look through this code for an example
+	      // of how to do that. 
+	      // ....
+
+	      var chosenCategoryShortName = chooseRandomCategory(categories);
+	      var short_name = chosenCategoryShortName.short_name;
+
+	      var finalHtml = homeHtml;
+
+	      finalHtml = insertProperty(finalHtml,"randomCategoryShortName",short_name);
+
+	      insertHtml("#main-content", finalHtml);
+	      
+	    },
+	    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+	}
+
+
+// Given array of category objects, returns a random category object.
+function chooseRandomCategory (categories) {
+  // Choose a random index into the array (from 0 inclusively until array length (exclusively))
+  var randomArrayIndex = Math.floor(Math.random() * categories.length);
+
+  // return category object with that randomArrayIndex
+  return categories[randomArrayIndex];
+}
+
+
 
 	/*LOAD MENU CATEGORIES*/
 	davidChu.loadMenuCategories = function () {
